6c2ae9fa15aa3d6d8836e576fbe65e70717700b5
neullxh21 <neullxh21@gmail.com>
2023-09-25 15:34:49 +0900

    week5 v1.1

--- week5/main.go
+++ week5/main.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("input score :")
-	reader := bufio.NewReader(os.Stdin)
-	inputScoreString, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	inputScoreString = strings.TrimSpace(inputScoreString)      // 공란 제거
-	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // casting
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	var grade string
-	if inputScore >= 90 {
-		grade := "A grade!"
-	} else {
-		grade := "BCDE grade~"
-	}
-	fmt.Println("You got" + grade)
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 }
